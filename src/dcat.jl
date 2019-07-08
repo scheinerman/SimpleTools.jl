3ac5f27cc06a7e8d93e91e1ab1730736996bccdf
Ed Scheinerman <ers@jhu.edu>
2019-07-08 10:18:25 -0400

    Improved dcat

--- src/dcat.jl
+++ src/dcat.jl
@@ -5,8 +5,8 @@ export dcat
 Returns a new matrix of the form `[A 0; 0 B]` where the `0`s
 are blocks of zeros of the approriate shape.
 
-More generally, may be called with any positive number of matrix
-arguments.
+More generally, may be called with any positive number of matrix, vector,
+or scalar (numeric) arguments.
 """
 function dcat(A::Array{S,2}, B::Array{T,2}) where {S,T}
     ST = promote_type(S,T)
@@ -21,9 +21,31 @@ function dcat(A::Array{S,2}, B::Array{T,2}) where {S,T}
 end
 
 
-dcat(A::Array{S,2}) where S = A
+dcat(A) where S = _matrixify(A)
 
-function dcat(A::Array{S,2}, B::Array{T,2}, C...) where {S,T}
+function dcat(A,B, C...)
     AB = dcat(A,B)
     return dcat(AB,C...)
+end
+
+
+function _matrixify(v::Array{T,1}) where T
+    n = length(v)
+    return reshape(v,n,1)
+end
+
+_matrixify(x::T) where T<:Number = _matrixify([x])
+
+_matrixify(A::Array{T,2}) where T = A
+
+function dcat(v::T, A::Matrix{S}) where {S,T}
+    return dcat(_matrixify(v),A)
+end
+
+function dcat(A::Matrix{S},v::T) where {S,T}
+    return dcat(A,_matrixify(v))
+end
+
+function dcat(x::T, y::S) where {S,T}
+    return dcat(_matrixify(x),_matrixify(y))
 end
